3b69a460ea5ca2c4e427dfb3076fda93a6031e04
eikeon <eikeon@f230b9f2-1df4-0310-83c7-9b8ba782e577>
2006-07-13 15:06:18 +0000

    updating to use new rdflib interfaces; added daemon option; improved usage of logging

--- redfoot-app/redfootlib/main.py
+++ redfoot-app/redfootlib/main.py
@@ -7,31 +7,26 @@ For help on redfoot see:
    redfoot.py --help
 """
 
-import sys, logging, traceback
-from optparse import OptionParser
-from redfootlib import __version__
-from redfootlib.install_rdflib import install_rdflib
+import optparse, logging
 
+parser = optparse.OptionParser(usage="usage: %prog [options] [args]")
 
-logging.basicConfig(level=logging.DEBUG, format='%(levelname)s %(message)s')
-
-logging.info("redfoot version: %s" % __version__)
-
-usage = "usage: %prog [options] [args]"
-parser = OptionParser(usage=usage)
-# where to doc what program_uri "program url"    
 parser.allow_interspersed_args = False
+
 parser.add_option("--install-rdflib", dest="install_rdflib", action="store_true", help="download and install latest stable rdflib or version specified by URL")
 parser.add_option("--update", action="store_true", dest="update", help="update program")    
 parser.add_option("--quiet", action="store_true", dest="quiet", help="")    
-parser.add_option("--program", dest="program", help="name of program program to load and run")
+parser.add_option("--program", dest="program", help="URIRef of program to load and run")
 parser.add_option("--program-args", dest="program_args", help="arguments for program program")
 parser.add_option("--set-default", action="store_true", dest="set_default", help="sets the current program as the default")
 parser.add_option("--clear-default", action="store_true", dest="clear_default", help="clears the current default program")
-parser.add_option("--backend", dest="backend", help="name of rdflib Graph backend to use")
+parser.add_option("--store", dest="store", help="name of rdflib store to use")
 parser.add_option("--path", dest="path", help="path to database")
+parser.add_option("--daemon", dest="daemon", action="store_true", help="run as a daemon")
+parser.add_option("--log-level", action="store", type="int", dest="log_level", help="logger level")
+parser.add_option("--name", action="store", type="string", dest="name", help="name to use as base for log and PID files")
 
-parser.set_defaults(path="__rfdb__", program = None, update=False, quiet=False, backend=None, install_rdflib=False, set_default=False, clear_default=False)
+parser.set_defaults(path="__rfdb__", program = None, update=False, quiet=False, store=None, install_rdflib=False, set_default=False, clear_default=False, daemon=False, log_level=logging.DEBUG, name="redfoot")
 
 
 def main(command=None):
@@ -40,35 +35,46 @@ def main(command=None):
     else:
         args = command.split()
         options, args = parser.parse_args(args)
+
+    _logger = logging.getLogger("redfoot")
+    _logger.setLevel(options.log_level)
+    _formatter = logging.Formatter('%(message)s')
+    _handler = logging.StreamHandler()
+    _handler.setFormatter(_formatter)
+    _logger.addHandler(_handler)
+
+    if options.daemon:
+        _logger.debug("daemonizing")
+        from redfootlib.daemonize import daemonize
+        from redfootlib import pid
+        pid_file = "%s.pid" % options.name
+        pid.check_file(pid_file)
+        daemonize()
+        pid.add_file(pid_file)
+        _logger.removeHandler(_handler)
+
+    from logging.handlers import RotatingFileHandler
+    _handler = RotatingFileHandler("%s.log" % options.name, maxBytes=1024*1024, backupCount=5)
+    _formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(pathname)s %(lineno)d\n%(message)s')
+    _handler.setFormatter(_formatter)
+    _logger.addHandler(_handler)
+
     if options.install_rdflib:
+        from redfootlib.install_rdflib import install_rdflib
         install_rdflib(args)
     else:
+        from redfootlib import __version__
+        _logger.info("redfoot version: %s" % __version__)
         from redfootlib.BootLoader import BootLoader
-        loader = BootLoader(backend=options.backend)
-        try:
-            loader.open(options.path,create=False)
-            len(loader)
-            print "Existing store opened / verified"
-        except:
-            print "Unable to open / verify store:"
-            import traceback
-            traceback.print_exc()
-            try:
-                loader.open(options.path,create=True)
-                print "New store created"
-            except Exception, e:
-                print "Unable to create new store"
-                loader.close
-                raise e
+        loader = BootLoader(store=options.store)
+        _logger.info("opening with store=%s and configuration=%s" % (options.store, options.path))
+        loader.open(options.path)
         try:
             loader.main(options, args)
-            loader.commit()
+        finally:
             loader.close()
-        except:
-            loader.rollback()
-            loader.close()
-    
-        return loader
+        if options.daemon:
+            pid.remove_file(pid_file)
 
 if __name__=="__main__":
     main()
