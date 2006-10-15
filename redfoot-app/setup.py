23ffca95cc947cc9f55b4bc919f32b3ab300e415
eikeon <eikeon@f230b9f2-1df4-0310-83c7-9b8ba782e577>
2006-10-15 18:58:34 +0000

    updated info

--- redfoot-app/setup.py
+++ redfoot-app/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'redfoot',
-    version = "2.0.0",
+    version = "2.1.0",
     description = "A hypercode program loader and runner",
     author = "Daniel Krech",
     author_email = "eikeon@eikeon.com",
@@ -10,7 +10,7 @@ setup(
     maintainer_email = "eikeon@eikeon.com",
     url = "http://redfoot.net/",
 
-    license = "http://redfoot.net/2005/08/20/redfoot-2.0.0/LICENSE",
+    license = "http://redfoot.net/2006/10/15/redfoot-2.1.0/LICENSE",
     platforms = ["any"],
     classifiers = ["Programming Language :: Python"],
     long_description = "A hypercode program loader and runner.",
