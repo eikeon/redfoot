c4d61dd30fda2926785a91db76bcfd1ac768f89e
eikeon <eikeon@f230b9f2-1df4-0310-83c7-9b8ba782e577>
2007-03-11 22:22:27 +0000

    ez_install bits

--- redfoot-app/setup.py
+++ redfoot-app/setup.py
@@ -1,8 +1,13 @@
-from distutils.core import setup
+from ez_setup import use_setuptools
+use_setuptools()
+
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
 
 setup(
     name = 'redfoot',
-    version = "2.1.0",
+    version = "2.1.1",
     description = "A hypercode program loader and runner",
     author = "Daniel Krech",
     author_email = "eikeon@eikeon.com",
@@ -16,7 +21,16 @@ setup(
     long_description = "A hypercode program loader and runner.",
 
     packages = ["redfootlib"],
-    scripts = ["bin/redfoot"],
+    #scripts = ["bin/redfoot"],
+
+    entry_points = {
+        'console_scripts': [
+            'redfoot = redfootlib.main:main',
+        ]
+    },
+
+    install_requires = ["rdflib", "zope.interface>=3.3.0", "kid>=0.9.5"],
+
     )
 
 
