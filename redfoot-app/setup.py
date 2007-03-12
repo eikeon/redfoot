efe987c2976eb664e314c5e96b887b312a1712dd
eikeon <eikeon@f230b9f2-1df4-0310-83c7-9b8ba782e577>
2007-03-12 15:31:12 +0000

    adding easy_install bits

--- redfoot-app/setup.py
+++ redfoot-app/setup.py
@@ -18,7 +18,13 @@ setup(
     license = "http://redfoot.net/2006/10/15/redfoot-2.1.0/LICENSE",
     platforms = ["any"],
     classifiers = ["Programming Language :: Python"],
-    long_description = "A hypercode program loader and runner.",
+    long_description = \
+    """A hypercode program loader and runner.
+
+    If you have recently reported a bug marked as fixed, or have a craving for
+    the very latest, you may want the development version instead:
+    https://svn.redfoot.net/trunk/redfoot-app#egg=redfoot-dev
+    """,
 
     packages = ["redfootlib"],
     #scripts = ["bin/redfoot"],
